a4862a366eb84eb3418fba7f1736309683f69238
Arunabh97 <dasarunabh036@gmail.com>
2024-01-11 20:59:46 +0530

    scroll section active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,6 +79,26 @@ tabs.forEach(tab => {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(`.nav__menu a[href*='${sectionId}']`).classList.add('active-link')
+        } else {
+            document.querySelector(`.nav__menu a[href*='${sectionId}']`).classList.remove('active-link')
+        }
+    })
+}
+
+// Attach scrollActive function to the scroll event
+window.addEventListener('scroll', scrollActive)
 
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
