5bd25593879be5033ff3e3ef1c85f4854c2728ff
Arunabh97 <dasarunabh036@gmail.com>
2024-01-14 20:26:26 +0530

    Some Fixes Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,10 +89,13 @@ function scrollActive() {
         const sectionTop = current.offsetTop - 50;
         let sectionId = current.getAttribute('id')
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector(`.nav__menu a[href*='${sectionId}']`).classList.add('active-link')
-        } else {
-            document.querySelector(`.nav__menu a[href*='${sectionId}']`).classList.remove('active-link')
+        const navLink = document.querySelector(`.nav__menu a[href*='${sectionId}']`);
+        if (navLink) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                navLink.classList.add('active-link');
+            } else {
+                navLink.classList.remove('active-link');
+            }
         }
     })
 }
